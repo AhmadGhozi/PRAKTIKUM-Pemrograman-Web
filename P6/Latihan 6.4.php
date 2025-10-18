2ad3caca26caee477fde1b9db6c040fadadd17c2
AhmadGhozi <72675510+AhmadGhozi@users.noreply.github.com>
2025-10-18 19:01:35 +0800

    Add files via upload

---
+++ P6/Latihan 6.4.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <?php
+        echo "<title>Halaman PHP Pertamaku</title>";
+        ?>
+    </head>
+    <body>
+        <h1>Script PHP dalam (X)HTML</h1>
+
+        <?php
+        echo "<p>Ini paragraph yang dibuat dengan script PHP</p>";
+        echo "<p>Ini paragraph yang dibuat dengan script PHP</p>";
+        ?>
+
+        <p>Ini paragraph yang dibuat dengan script PHP</p>
+        <p>Ini paragraph yang dibuat dengan script PHP</p>
+
+        <?php
+        echo "<p>Ini paragraph yang dibuat dengan script PHP</p>";
+        echo "<p>Ini paragraph yang dibuat dengan script PHP</p>";
+        ?>
+    </body>
+</html>
---
+++ P6/Latihan 6.7.php
@@ -0,0 +1,11 @@
+<?php
+echo "Hello World 1! </br>";
+?>
+
+<?php
+echo "Hello World 2! </br>";
+?>
+
+<?php
+echo "Hello World 3!";
+?>
