7f59471f995998565db2d9cde5b31028cdabab32
AhmadGhozi <72675510+AhmadGhozi@users.noreply.github.com>
2025-10-27 21:20:59 +0800

    Add files via upload

---
+++ P7/Latihan 7.2-2.php
@@ -0,0 +1,10 @@
+<?php
+    $nama = ["Andri","joko","Sukma","Rina","Sari"];
+
+    echo $nama[1];
+    echo "<br />";
+    echo $nama[2];
+    echo "<br />";
+    echo $nama[3];
+    echo "<br />";
+?>
---
+++ P7/Latihan 7.4-1.php
@@ -0,0 +1,13 @@
+<?php
+$penjumlahan = 2 + 4;
+$pengurangan = 6 - 2;
+$perkalian = 5 * 3;
+$pembagian = 15 / 3;
+$modulus = 5 % 2;
+
+echo "hasil: 2 + 4 = ". $penjumlahan."<br>";
+echo "hasil: 6 - 2 = ". $pengurangan."<br>";
+echo "hasil: 5 * 3 = ". $perkalian."<br>";
+echo "hasil: 15 / 3= ". $pembagian."<br>";
+echo "hasil: 5 % 2 = ". $modulus."<br>";
+?>
